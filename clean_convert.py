14ce1dde20ce315f9c30a4eb599ca964b2552e5e
Munger <Wade_Munger@Dell.com>
2018-06-20 14:59:18 -0500

    updated script

---
+++ clean_convert.py
@@ -0,0 +1,72 @@
+# -*- coding: cp1252 -*-
+#round 2
+
+import csv
+
+
+def main():
+    dic_of_coors = make_dict()
+    test(dic_of_coors)
+    write_to_geojson(dic_of_coors)
+
+
+def write_to_geojson(a_dict):
+    geo_file = open("mpas.geojson","w")
+    geo_file.write('{\n')#  {
+    geo_file.write('"type": FeatureCollection",\n')
+    geo_file.write('"features": [\n')#[
+    geo_file.write('{\n')#  {
+    geo_file.close()
+
+
+def make_dict():
+    dumb_list = ["33° 33.224' N. lat. 117° 49.184' W. long.;","33° 30.211' N. lat. 117° 49.200' W. long.;","33° 30.713' N. lat. 117° 49.200' W. long.; and","33° 30.713' N. lat. 117° 45.264' W. long."]
+
+    lat = ""
+    sub_lat=''
+    lon = ""
+    sub_lon=''
+    key = 0
+    coor_list=[]
+
+    coor_dict = {}
+
+    for value in dumb_list:
+        #handling lat
+        lat = value[0:10]
+        sub_lat = lat[4:10]
+        sub_lat = round((float(sub_lat)/60),6)
+        lat = lat[0:2]
+        lat = float(lat)
+        lat = lat + sub_lat
+
+
+        #handling lon
+        lon = value[20:40]
+        sub_lon = lon[5:11]
+        sub_lon = round(((float(sub_lon))/60),6)
+        lon = lon[0:3]
+        lon = float(lon)+sub_lon
+        lon = lon * -1
+
+        #adding both to a dict. Key auto generated
+        coor_list = [lon,lat]
+        coor_dict[key] = coor_list
+        key+=1
+    return coor_dict
+
+def write_to_csv(some_dict):
+    key = 0
+    with open('dict.csv','wb') as csv_file:
+        writer = csv.writer(csv_file)
+        for i in some_dict:
+            writer.writerow(some_dict[key])
+            key+=1
+
+def test(some_dict):
+    k = 0
+    for i in some_dict:
+        print some_dict[k]
+        k+=1
+
+main()
